19287a957726dea1ecf89c460830b2c97af78c9f
olujuide Oyewole <ooye359@mail.midland.edu>
2017-09-18 17:33:08 -0500

    hello world

---
+++ Week 2/Week_2.groovy
@@ -0,0 +1,19 @@
+
+//Commit 1 - printing hello world
+//printing "hello world"
+
+//Commit 2 - constants
+class Constants {
+final String FIRST_NAME = "Olujide"
+String lastName = "Oyewole"
+}
+//Commit 3 - Numeric variables
+/**
+integer
+BigDecimal
+Foat
+**/
+Integer leet = 1337
+BigDecimal result = leet / 47
+
+print leet - result
